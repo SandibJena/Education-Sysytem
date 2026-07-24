5c98c598926f584f5ed258c0201c1288ccd6f79a
SandibJena <jenasandib@gmail.com>
2026-07-24 15:57:06 +0530

    Changes in source,modeule and student

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,23 @@
 const express = require("express");
-const cookieParser = require("cookie-parser"); 
+const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const { handle404Error, handleGlobalError, } = require("./middlewares");
+const { handle404Error, handleGlobalError } = require("./middlewares");
 const { v1Routes } = require("./routes/v1");
 const { cors } = require("./config");
 const path = require("path");
+
 const app = express();
 
-app.use(cors)
+app.use((req, res, next) => {
+    console.log("Incoming Request:", req.method, req.originalUrl);
+    next();
+});
+
+app.use(cors);
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(cookieParser());
 
 app.use("/api/v1", v1Routes);
